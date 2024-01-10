f87395fe8e633179e2e5d0e3f9125ea2829eb60c
losirlu1411 <148865607+losirlu1411@users.noreply.github.com>
2024-01-10 16:56:03 +0000

    Added parameter and docstring with Description

--- 22081216.py
+++ 22081216.py
@@ -9,47 +9,86 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.ticker as ticker
-
 from matplotlib.gridspec import GridSpec
 
-# Plot 1 Sales and Profit per Monthly
-def plot_line(df, grid):
-    plt.subplot(grid[0 , 0])
-    #print( df.columns)
-    month_Sales = df.groupby(['year','month'])['Sales'].sum().reset_index()
-    month_Profit = df.groupby(['year','month'])['Profit'].sum().reset_index()
-    month_Sales['year_month'] = month_Sales['year'].astype(str) + '-' + month_Sales['month'].astype(str)
-    month_Profit['year_month'] = month_Profit['year'].astype(str) +'-' + month_Profit['month'].astype(str)
-# Set the figure Size
-    
-    ax= sns.lineplot(data=month_Sales, x="year_month", y="Sales", label = "Sales")
-    ax = sns.lineplot(data=month_Profit, x="year_month", y="Profit", label = "Profit")
+# Plot 1 Sales and Profit Trends across years and months.
+
+
+def plot_line(store, grid):
+    """
+    It will takes the sales and profit from csv file and plot the line graph
+    according to the rescpective year and month.
+
+    Parameters
+    ----------
+    store : It is a csv file containing the store data
+        It will read the csv file containing the data of the store.
+    grid : It is a Subplot type
+        It is used for specifying the grid position.
+
+    Returns
+    -------
+    None.
+
+    """
+    # It will produce subplot for sales and trends accross years and months.
+    plt.subplot(grid[0, 0])
+    # grouping the sales and profit according to month and year.
+    month_sales = store.groupby(['year', 'month'])['Sales'].sum().reset_index()
+    month_profit = store.groupby(['year', 'month'])[
+        'Profit'].sum().reset_index()
+    month_sales['year_month'] = month_sales['year'].astype(str) + '-' \
+        + month_sales['month'].astype(str)
+    month_profit['year_month'] = month_profit['year'].astype(str) + '-'\
+        + month_profit['month'].astype(str)
+    # Set the figure Size.
+    a_x = sns.lineplot(data=month_sales, x="year_month", y="Sales",
+                       label="Sales")
+    a_x = sns.lineplot(data=month_profit, x="year_month", y="Profit",
+                       label="Profit")
+    # plot the xlabel, ylabel and title.
     plt.xlabel("Year and month")
     plt.ylabel("Profit and Sales(USD)")
     plt.title("Sales and Profit Trends Across Years & Months")
-    ax.xaxis.set_major_locator(ticker.MultipleLocator(6))
-    #plt.legend()
-    #fig.show()
-    #plt.show()
-  
+    # It is major ticks on the x-axis should be located at multiples of 6.
+    a_x.xaxis.set_major_locator(ticker.MultipleLocator(6))
 
 
-# plot 2 Quantity by Category and Segment
-def bar_plot_v(df, grid):
-    plt.subplot(grid[0 , 1])
-    df = pd.DataFrame(df)
+# plot 2 Quantity by Category and Segment.
+def bar_plot_v(store, grid):
+    """
+    It will takes the category and segment with respective to Quantity  
+    from csv file and plot the bar graph.
+
+    Parameters
+    ----------
+    store : It is a csv file containing the store data.
+        It will read the csv file containing Quantity,Category and Segment 
+        data of the store.
+    grid :  It is a Subplot type
+        It is used for specifying the grid position.
+
+    Returns
+    -------
+    None.
+
+    """
+    plt.subplot(grid[0, 1])
+    store = pd.DataFrame(store)
 
     # Group by 'Category' and 'Segment' and sum the 'Quantity'
-    grouped_df = df.groupby(['Category', 'Segment'])[['Quantity']].sum().reset_index()
+    grouped_store = store.groupby(['Category', 'Segment'])[
+        ['Quantity']].sum().reset_index()
 
     # Create a bar plot using plt.bar
-    categories = grouped_df['Category'].unique()
+    categories = grouped_store['Category'].unique()
     bar_width = 0.15  # Width of each bar
     index = range(len(categories))
 
-    for i, segment in enumerate(grouped_df['Segment'].unique()):
-        segment_data = grouped_df[grouped_df['Segment'] == segment]
-        plt.bar([pos + i * bar_width for pos in index], segment_data['Quantity'], width=bar_width, label=segment)
+    for i, segment in enumerate(grouped_store['Segment'].unique()):
+        segment_data = grouped_store[grouped_store['Segment'] == segment]
+        plt.bar([pos + i * bar_width for pos in index],
+                segment_data['Quantity'], width=bar_width, label=segment)
 
     # Adding labels and title
     plt.legend(loc='upper left')
@@ -59,81 +98,132 @@ def bar_plot_v(df, grid):
 
     # Adding x-axis labels and legend
     plt.xticks([pos + bar_width for pos in index], categories)
-    #plt.legend()
+# plot 3 Purchases rates in Top 10 USA States.
+def pie_plot(store, grid):
+    """
+    It will takes the purchases  with respective to top 10 states 
+    from csv file and plot the pie  graph.
 
-    # Show the plot
-    #plt.show()
-    
-#plot 3
-def pie_plot(df, grid):
-    plt.subplot(grid[0 , 2])
-    temp=pd.DataFrame(df.groupby('State')['Quantity'].sum().reset_index())
-    temp = temp.sort_values('Quantity', ascending=False).head(10)
-    explode = [0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0]   
-    plt.pie(temp['Quantity'], labels=temp['State'], explode=explode, autopct='%1.1f%%')
-    plt.title('Purchase rate in different states')
+    Parameters
+    ----------
+    store : It is a csv file containing the store data.
+        It will read the csv file containing top 10 USA States from 
+        data of the store.
+    grid :  It is a Subplot type
+        It is used for specifying the grid position.
 
-#plot 4
-def bar_plot(df, grid):
-    ax = plt.subplot(grid[1 , 2])
-# Creating a DataFrame with sub-category sales data
-    df = pd.DataFrame(df)
+    Returns
+    -------
+    None.
 
-    sub_df = df.groupby('Sub-Category', as_index=False)['Sales'].sum().sort_values(by='Sales')
+    """
+    # It will produce subplot for Top 10 States with highest purchase rate.
+    plt.subplot(grid[0, 2])
+    # Group the data to filter State with Quantity
+    store = pd.DataFrame(store.groupby('State')[
+                         'Quantity'].sum().reset_index())
+    # Filter the data to get top 10 purchase State.
+    store = store.sort_values('Quantity', ascending=False).head(10)
+    # Explode the pie Chart
+    explode = [0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # Plot the pie plot
+    plt.pie(store['Quantity'], labels=store['State'], explode=explode,
+            autopct='%1.1f%%')
+    # Add the title
+    plt.title('Purchase rate in top 10 USA States')
 
-    ax.barh(sub_df['Sub-Category'], sub_df['Sales'])
-    ax.set_xlabel('Sales')
-    ax.set_ylabel('Sub-Category')
-    ax.set_title('Sales by Sub-Category')
+# plot 4
+def bar_plot(store, grid):
+    """
+    It will takes the Sub-Category with Sales data from csv file and
+    plot the pie  graph.
+    Parameters
+    ----------
+    store : It is a csv file containing the store data.
+        It will read the csv file containing  Sub_categories with Sales data
+        from the data set.
+    grid :  It is a Subplot type
+        It is used for specifying the grid position.
+
+    Returns
+    -------
+    None.
+
+    """
+    data_store = plt.subplot(grid[1, 2])
+    # Creating a DataFrame with sub-category sales data.
+    store = pd.DataFrame(store)
+    # Filter the data.
+    sub_store = store.groupby('Sub-Category',
+                        as_index=False)['Sales'].sum().sort_values(by='Sales')
+    # Plot the bar.
+    data_store.barh(sub_store['Sub-Category'], sub_store['Sales'])
+    # Add the Labels.
+    data_store.set_xlabel('Sales')
+    data_store.set_ylabel('Sub-Category')
+    # add the tile.
+    data_store.set_title('Sales by Sub-Category')
+
 
 def print_detail():
-    # Add a subplot for key insights
-    plt.subplot(grid[1 , 1])
+    """
+    It is used to highlights points and name and Student ID.
+    Returns
+    -------
+    None.
+
+    """
+    # Add a subplot for highlights.
+    # Plot the grid for Highlights.
+    plt.subplot(grid[1, 1])
+    # Add the Highlights of the Dashboard.
     text = "Highlights:\n\n" \
            "1. Profit is less  with the Sales compared\n"\
-               "accross the Superstore.\n" \
+        "accross the Superstore.\n" \
            "2. Highest demand for office supplies is \n"\
-               "from the consumer segment.\n" \
+        "from the consumer segment.\n" \
            "3.California has the highest purchase \n"\
-               " among the top 10 cities in USA \n" \
+        " among the top 10 cities in USA \n" \
            "4. Phones and Chairs are the highest \n" \
            "   Sub -category product from the Sales."
-    plt.text(0.5 , 0.5 , text , ha='center' , va='center' , fontsize=12 ,
-             color='gray' , fontstyle='italic' , fontweight='bold')
+    # Add Text format of Highlights.
+    plt.text(0.5, 0.5, text, ha='center', va='center', fontsize=12,
+              fontstyle='italic', fontweight='bold')
     plt.axis('off')
 
-    # Add student information
-    plt.subplot(grid[1 , 0])
+    # Add Student Name and ID.
+    plt.subplot(grid[1, 0])
     text = "Student Name: Changdra Shekar Losirlu\n" \
            "Student ID: 22081216"
-    plt.text(0.5 , 0.5 , text , ha='center' , va='center' , fontsize=12 , color='gray' , fontweight='bold')
+    # Add Text format of Studend name and ID.
+    plt.text(0.5, 0.5, text, ha='center', va='center',
+             fontsize=12, fontweight='bold')
     plt.axis('off')
 
-    # Main title for the entire dashboard
-    fig.suptitle('Superstore Sales and Profit  Analysis Dashboard' , fontsize=25 , fontweight='bold')
-    
+    # Main title for the entire dashboard.
+    fig.suptitle('Superstore Sales and Profit  Analysis Dashboard',
+                 fontsize=25, fontweight='bold')
+
 
 #######################Main ##########################
 
 
-df = pd.read_csv("Sample - Superstore.csv")
-df['Order Date'] = df['Order Date'].str.replace('-', '/')
-df['Ship Date'] = df['Ship Date'].str.replace('-', '/')
-df['Order Date'] = pd.to_datetime(df['Order Date'], format='%m/%d/%Y')
-df['Ship Date'] = pd.to_datetime(df['Ship Date'], format='%m/%d/%Y')
-df['year']=df['Order Date'].dt.year
-df['month'] = df['Order Date'].dt.month
-sns.set_theme()
-fig = plt.figure(figsize=(18 , 10),dpi=300)
-grid = GridSpec(2 , 3 , width_ratios=[1 , 1 , 1] , height_ratios=[1 , 1])
-plt.savefig('22081216.png' , dpi=300)
+store = pd.read_csv("Sample - Superstore.csv")
+store['Order Date'] = store['Order Date'].str.replace('-', '/')
+store['Ship Date'] = store['Ship Date'].str.replace('-', '/')
+store['Order Date'] = pd.to_datetime(store['Order Date'], format='%m/%d/%Y')
+store['Ship Date'] = pd.to_datetime(store['Ship Date'], format='%m/%d/%Y')
+store['year'] = store['Order Date'].dt.year
+store['month'] = store['Order Date'].dt.month
+#Plot the Dashboard
+fig = plt.figure(figsize=(18, 10), dpi=300)
+grid = GridSpec(2, 3, width_ratios=[1, 1, 1], height_ratios=[1, 1])
 
-
-plot_line(df,grid)
-bar_plot_v(df,grid)
-pie_plot(df,grid)
-bar_plot(df,grid)
+plot_line(store, grid)
+bar_plot_v(store, grid)
+pie_plot(store, grid)
+bar_plot(store, grid)
 print_detail()
-plt.savefig('22081216.png' , dpi=300)
-
-plt.show()
+#Save the Image with ID.png
+plt.savefig('22081216.png', dpi=300)
+# plt.show()
